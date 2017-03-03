526aa8742a1dfd34c5e8e6a3ee4443f4440062a5
Asser Hamad <asserhamad96@gmail.com>
2017-03-03 23:37:25 +0200

    I'm tired

--- server.js
+++ server.js
@@ -5,24 +5,27 @@ var mongojs=require('mongojs');
 var mongoose=require('mongoose');
 var path = require('path');
 var fs = require('fs');
-var s_info = mongojs('s_info',['s_info','s_portfolio','s_links']);
+var s_info = mongojs('s_info',['s_info','s_portfolio','s_links','s_screenshots']);
 var multer = require('multer');
 var upload=multer({dest:'uploads/'});
 
-app.post('/upload',upload.any(),function(req,res){
-  console.log(JSON.stringify(req.body)+"   "+JSON.stringify(req.files));
+app.post('/upload/:username',upload.any(),function(req,res){
+  s_info.s_screenshots.insert({username : req.params.username, path : req.files[0].path},function(err,doc){
+    console.log(JSON.stringify(doc));
+  });
   //res.send(req.files);
 });
 
-app.get('/links/:username',function(req,res){
-  res.json(s_info.s_links.find({username : req.params.username}));
+app.get('/screenshots/:username',function(req,res){
+  res.json(s_info.s_screenshots.find({username : req.params.username}));
+});
+//app.post('/damn',function(req,res){
+//  if(s_info.s_portfolio.findOne({username : req.data.username})==null)s_info.s_portfolio.insert(req.body);
+//});
+app.post('/link',function(req,res){
+  s_info.s_links.insert(req.body);
 });
 
-app.post('/link',function(req,res){
-  s_info.s_links.insert(req.body,function(err,doc){
-    res.json(doc);
-  });
-});
 //app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -52,7 +55,6 @@ app.post('/login',function(req,res){
 });
 
 app.get('/done/:username',function(req,res){
-  console.log("hey bro I'm redirecting you now dw");
   res.render('portfolio');
 });
 app.get('/done/porto/:username',function(req,res){
